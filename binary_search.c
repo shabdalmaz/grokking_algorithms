76df924dc2cd0f89156bf61cb7653144b00bdc7c
shabdalmaz <ashabdan@student.42.us.org>
2020-04-05 17:49:08 -0700

    recursive reproducing is fixed

--- binary_search.c
+++ binary_search.c
@@ -6,9 +6,7 @@ int		binary_search(int list[], size_t list_len, int item)
 	int		high;
 	int		mid;
 	int		guess;
-	int		steps;
 
-	steps = 0;
 	low = 0;
 	high = list_len - 1;
 	mid = 0;
@@ -18,39 +16,42 @@ int		binary_search(int list[], size_t list_len, int item)
 		mid = (low + high) / 2;
 		guess = list[mid];
 		if (guess == item)
-		{
-			printf("steps - %d\n", steps);
 			return (mid);
-		}
 		else if (guess > item)
 			high = mid - 1;
 		else
 			low = mid + 1;
-		steps += 1;
 	}
 	return (-1);
 }
 
-int		recur_bin_search(int *list, size_t size, int x)
+int		recur_bin_search(int *list, int x, int low, int high)
 {
-	int		low;
-	int		high;
+	int	mid;
 
-	low = 0;
-	high = size - 1;
-	if (list[high] == x)
-		return (high);
-	return (recur_bin_search(list, (low + high) / 2, x));
+	mid = (low + high) / 2;
+	if (list[mid] == x)
+		return (mid);
+	else if (low > high && list[mid] != x)
+		return (-1);
+
+	if (list[mid] > x)
+		high = mid - 1;
+	else if (list[mid] < x)
+		low = mid + 1;
+	return (recur_bin_search(list, x, low, high));
 }
 
 int		main(void)
 {
 	int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 	int res;
+	int x = 11;
 
-	res = binary_search(list, sizeof(list) / sizeof(list[0]), 9);
-	printf("index of %d is %d\n", list[res], res);
-	res = recur_bin_search(list, sizeof(list) / sizeof(list[0]), 9);
-	printf("index of %d is %d\n", list[res], res);
+	res = binary_search(list, sizeof(list) / sizeof(list[0]), x);
+	printf("index of %d is %d\n", x, res);
+	
+	res = recur_bin_search(list, x, 0, sizeof(list) / sizeof(list[0]) - 1);
+	printf("index of %d is %d\n", x, res);
 	return (0);
 }
